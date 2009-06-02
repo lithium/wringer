53813bb9389a2bb124f559b685720283b7a4c998
lithium <wothin@gmail.com>
2009-06-01 22:31:55 -0700

    initial

---
+++ src/com/hlidskialf/android/wringer/WringerActivity.java
@@ -0,0 +1,15 @@
+package com.hlidskialf.android.wringer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WringerActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
